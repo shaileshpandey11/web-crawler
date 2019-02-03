1e3f6e1b52bfdf76faec663be1f4f01c207b4ed7
Shailesh Pandey <shailesh11july@gmail.com>
2019-02-03 11:12:47 +0530

    Add main class for web crawling

---
+++ src/main/java/com/prudential/web/crawler/CrawlerMain.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.prudential.web.crawler;
+
+/**
+ * This main class is responsible for crawling the World Wide Web, 
+ * typically for the purpose of Web indexing (web spidering).
+ *
+ */
+public class CrawlerMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
