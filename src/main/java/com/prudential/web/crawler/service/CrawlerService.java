ded314ae8635f712ac082a416252ef3b01ec929c
Shailesh Pandey <shailesh11july@gmail.com>
2019-02-03 13:38:56 +0530

    Proper indentation

--- src/main/java/com/prudential/web/crawler/service/CrawlerService.java
+++ src/main/java/com/prudential/web/crawler/service/CrawlerService.java
@@ -17,61 +17,62 @@ import java.util.concurrent.TimeUnit;
  *
  */
 public class CrawlerService {
-	
+
 	/**
 	 * List of unique URLs to be crawled
 	 */
 	private Set<URL> crawledURLs = Collections.synchronizedSet(new HashSet<>());
-    /**
-     * Thread which will parse and extracts result from domain
-     */
-    private final ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-    /**
-     * Final output after execution 
-     */
-    private final List<Future<Set<URL>>> output = new ArrayList<>();
-    
-    /**
-     * This method submit tasks to executor service
-     * 
-     * @param urlToBeCrawled URL to be crawled
-     */
-    public void crawl(final String urlToBeCrawled) throws IllegalArgumentException{
-    	
-    	if(urlToBeCrawled==null) {
-    		throw new IllegalArgumentException();
-    	}
-    	try {
+	/**
+	 * Thread which will parse and extracts result from domain
+	 */
+	private final ExecutorService executorService = Executors
+			.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+	/**
+	 * Final output after execution
+	 */
+	private final List<Future<Set<URL>>> output = new ArrayList<>();
+
+	/**
+	 * This method submit tasks to executor service
+	 * 
+	 * @param urlToBeCrawled URL to be crawled
+	 */
+	public void crawl(final String urlToBeCrawled) throws IllegalArgumentException {
+
+		if (urlToBeCrawled == null) {
+			throw new IllegalArgumentException();
+		}
+		try {
 			addAndSubmitURL(new URL(urlToBeCrawled));
 			executorService.shutdown();
-            executorService.awaitTermination(5, TimeUnit.SECONDS);
+			executorService.awaitTermination(5, TimeUnit.SECONDS);
 		} catch (MalformedURLException e) {
-			System.out.println("URL is malformed"+e.getMessage());
+			System.out.println("URL is malformed" + e.getMessage());
 			e.printStackTrace();
 		} catch (InterruptedException e) {
-			System.out.println("An error ouccred while crawling URL"+e.getMessage());
+			System.out.println("An error ouccred while crawling URL" + e.getMessage());
 			e.printStackTrace();
 		}
-    
-    }
-    
-    /**
-     * This method adds URLs in collection and
-     * sends the URL to be crawled to the executor service.
-     *
-     * @param url URL to be crawled
-     */
-    private void addAndSubmitURL(URL url) {
 
-    	crawledURLs.add(url);
+	}
 
-    	output.add(executorService.submit(new WebCrawlerExecutorService(url)));
-    } 
-    
-    /**
-     * @return Crawled URLs
-     */
-    public Set<URL> getCrawledURLs() {
-        return new HashSet<>(this.crawledURLs);
-    }
+	/**
+	 * This method adds URLs in collection and sends the URL to be crawled to the
+	 * executor service.
+	 *
+	 * @param url URL to be crawled
+	 */
+	private void addAndSubmitURL(URL url) {
+
+		crawledURLs.add(url);
+
+		output.add(executorService.submit(new WebCrawlerExecutorService(url)));
+	}
+
+	/**
+	 * @return Crawled URLs
+	 */
+	public Set<URL> getCrawledURLs() {
+		return new HashSet<>(this.crawledURLs);
+	}
 }
--- src/main/java/com/prudential/web/crawler/service/WebCrawlerExecutorService.java
+++ src/main/java/com/prudential/web/crawler/service/WebCrawlerExecutorService.java
@@ -5,17 +5,16 @@ import java.util.Set;
 import java.util.concurrent.Callable;
 
 /**
- * This class will be responsible for parsing URL and extracting
- * results
+ * This class will be responsible for parsing URL and extracting results
  *
  */
-public class WebCrawlerExecutorService implements Callable<Set<URL>>{
-	
+public class WebCrawlerExecutorService implements Callable<Set<URL>> {
+
 	private final URL url;
-	
+
 	public WebCrawlerExecutorService(final URL url) {
-        this.url = url;
-    }
+		this.url = url;
+	}
 
 	@Override
 	public Set<URL> call() throws Exception {
